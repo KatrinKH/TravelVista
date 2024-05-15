10b59976e1dd1513361aefb785bc512fec01f1f6
NIX <113206190+spl1ntero@users.noreply.github.com>
2024-05-15 12:24:54 +0300

    Delete counter.js

--- counter.js
+++
@@ -1,3 +0,0 @@
-let counterEl = document.getElementById('counter');
-// console.log(counterEl);
-counterEl.innerHTML = "Нас сейчас смотрят " + (10 + Math.floor(Math.random() * 1000)) + " " + "пользователей"
